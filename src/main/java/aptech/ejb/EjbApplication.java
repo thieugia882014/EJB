5d803bbfd81233c5d739c916bf82d5e1c1456322
diepminhtuan <tuandmth2007019@fpt.edu.vn>
2022-05-27 08:59:52 +0700

    Initial commit

---
+++ src/main/java/aptech/ejb/EjbApplication.java
@@ -0,0 +1,13 @@
+package aptech.ejb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aptech/ejb/EjbApplicationTests.java
@@ -0,0 +1,13 @@
+package aptech.ejb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
